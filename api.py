94354663a29d41154259176b6f289e1805ad985d
Mr. Ryan <60050719+ryanhhogan3@users.noreply.github.com>
2023-09-24 16:50:22 -0500

    test

--- api.py
+++ api.py
@@ -92,5 +92,5 @@ def get_json():
 # api.add_resource(TagsByYear, '/<ticker>/<year>')
 
 
-# if __name__ == '__main__':
-#     app.run(debug=True)
+if __name__ == '__main__':
+    app.run(debug=True)
