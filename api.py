fd23c0d038fa3ce8fe307c29371cdb04760fa0e2
Mr. Ryan <60050719+ryanhhogan3@users.noreply.github.com>
2023-09-24 20:09:53 -0500

    trial6

--- api.py
+++ api.py
@@ -4,8 +4,8 @@ from flask_cors import CORS
 
 
 
-# import pandas as pd
-# import pymongo
+import pandas as pd
+import pymongo
 
 ## PUSH 
 # git push heroku main
@@ -16,65 +16,65 @@ api = Api(app)
 CORS(app)
 
 # CLASS OBJECT TO HANDLE METHODS OF READING DATA FROM PYMONGO
-# class DataBase():
-#     # READING CONTENTS OF THE DATA BASE FOR SELECTED TICKER
-#     def getMasterFrame(self, input_ticker):
+class DataBase():
+    # READING CONTENTS OF THE DATA BASE FOR SELECTED TICKER
+    def getMasterFrame(self, input_ticker):
 
-#         ticker = input_ticker.upper()
+        ticker = input_ticker.upper()
 
-#         print(f'Searching database for {ticker}...')
+        print(f'Searching database for {ticker}...')
 
-#         client = pymongo.MongoClient("mongodb+srv://ryanhhogan:1314red1314red@cluster0.t0ayqhj.mongodb.net/")
+        client = pymongo.MongoClient("mongodb+srv://ryanhhogan:1314red1314red@cluster0.t0ayqhj.mongodb.net/")
 
-#         client.list_database_names()
+        client.list_database_names()
 
-#         stock = client[ticker]
+        stock = client[ticker]
 
-#         ann_tag_string = f'{ticker}_Annual_Tags'
+        ann_tag_string = f'{ticker}_Annual_Tags'
 
-#         collection = stock[ann_tag_string]
+        collection = stock[ann_tag_string]
 
-#         df_accounts = pd.DataFrame(list(collection.find()))
+        df_accounts = pd.DataFrame(list(collection.find()))
 
-#         print('Success')
+        print('Success')
 
-#         return df_accounts
+        return df_accounts
     
-#     # CONVERTS THE DATAFRAME RETRIEVED IN getMasterFrame FUNCTION AND CONVERTS IT TO JSON FOR API USE
-#     def ConvertMasterFrameToJSON(self, input_ticker):
+    # CONVERTS THE DATAFRAME RETRIEVED IN getMasterFrame FUNCTION AND CONVERTS IT TO JSON FOR API USE
+    def ConvertMasterFrameToJSON(self, input_ticker):
         
-#         annual_tags_df = self.getMasterFrame(input_ticker)
+        annual_tags_df = self.getMasterFrame(input_ticker)
 
-#         annual_tags_df = annual_tags_df.drop(['_id'], axis=1)
+        annual_tags_df = annual_tags_df.drop(['_id'], axis=1)
 
-#         json_data = annual_tags_df.to_dict(orient='records')
+        json_data = annual_tags_df.to_dict(orient='records')
 
-#         return jsonify(json_data)
+        return jsonify(json_data)
     
-#     def getTagsByYear(self, input_ticker, year):
+    def getTagsByYear(self, input_ticker, year):
         
-#         annual_tags_df = self.getMasterFrame(input_ticker)
+        annual_tags_df = self.getMasterFrame(input_ticker)
 
-#         annual_tags_df = annual_tags_df.drop(['_id'], axis=1)
+        annual_tags_df = annual_tags_df.drop(['_id'], axis=1)
 
-#         parse_by_year = annual_tags_df.where(annual_tags_df['frame'].str.contains(str(year)) & (~annual_tags_df['frame'].str.contains("Q"))).dropna(how='all')
+        parse_by_year = annual_tags_df.where(annual_tags_df['frame'].str.contains(str(year)) & (~annual_tags_df['frame'].str.contains("Q"))).dropna(how='all')
 
-#         json_data = parse_by_year.to_dict(orient='records')
+        json_data = parse_by_year.to_dict(orient='records')
 
-#         return jsonify(json_data)
+        return jsonify(json_data)
 
 
-# class FinanceTags(Resource):
-#     def get(self, ticker):
-#         db_get_request = DataBase()
-#         request_in_json = db_get_request.ConvertMasterFrameToJSON(ticker)
-#         return request_in_json
+class FinanceTags(Resource):
+    def get(self, ticker):
+        db_get_request = DataBase()
+        request_in_json = db_get_request.ConvertMasterFrameToJSON(ticker)
+        return request_in_json
     
-# class TagsByYear(Resource):
-#     def get(self, ticker, year):
-#         db_connection = DataBase()
-#         request_in_json = db_connection.getTagsByYear(ticker, year)
-#         return request_in_json
+class TagsByYear(Resource):
+    def get(self, ticker, year):
+        db_connection = DataBase()
+        request_in_json = db_connection.getTagsByYear(ticker, year)
+        return request_in_json
     
 class status(Resource):    
      def get(self):
@@ -86,9 +86,9 @@ class other_route(Resource):
          
 
 api.add_resource(status, '/')
-api.add_resource(other_route, '/other')
-# api.add_resource(FinanceTags, '/<ticker>')
-# api.add_resource(TagsByYear, '/<ticker>/<year>')
+
+api.add_resource(FinanceTags, '/<ticker>')
+api.add_resource(TagsByYear, '/<ticker>/<year>')
 
 
 if __name__ == '__main__':
