eedc94fa2d18ad8ba8c6a9e59cc2b0628b9a5d98
Mr. Ryan <60050719+ryanhhogan3@users.noreply.github.com>
2023-11-11 21:18:19 -0600

    SEC endpoints

--- api.py
+++ api.py
@@ -1,22 +1,121 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, make_response
 from flask_restful import Resource, Api
 from flask_cors import CORS
 import yfinance as yf
 import pandas as pd
 from functools import lru_cache
 import time
+import requests
+
+# SET HEADERS FOR SEC REQUESTS
+headers = {'User-Agent': 'ben@gmail.com'}
 
 app = Flask(__name__)
 api = Api(app)
 CORS(app)
 
-###################################
-########## CLASS OBJECTS ##########
-###################################
+#######################################################################################################################################
+############################################################ EQUITY OBJECT FUNCTIONS ##################################################
+#######################################################################################################################################
 
 class Equity:
     def __init__(self, ticker):
         self.ticker = ticker
+
+
+###########################################################################
+######################### MISC AND NAME FUNCTIONS #########################
+
+    def getCIKDataFrame(self):
+        # REQUESTS THE COMAPNY TICKERS FROM SEC EDGAR API
+        tickers_cik = requests.get("https://www.sec.gov/files/company_tickers.json", headers=headers)
+        # NORMALIZES THE JSON DATA AND VALUES 
+        tickers_cik = pd.json_normalize(pd.json_normalize(tickers_cik.json(), max_level=0).values[0])
+        # CONVERTS CIK INT TO A STRING
+        tickers_cik["cik_str"] = tickers_cik["cik_str"].astype(str).str.zfill(10)
+        # SETS INDEX OF TICKERS_CIK DATAFRAME
+        tickers_cik.set_index("ticker",inplace=True)
+        # RETURNS DATAFRAME
+        return tickers_cik
+    
+    def getTickerCIK(self):
+        # CONVERTS THE INPUT TICKER TO ALL UPPERCASE
+        ticker = self.ticker.upper()
+        # TRY STATEMENT TO BETTER HANDLE ERRORS
+        try:
+            # GET CIK DATAFRAME, GETCIKDATAFRAME MUST BE DEFINED PRIOR TO CALL
+            data = self.getCIKDataFrame()
+            # CREATE DATAFRAME FROM CIK_STR ONLY
+            CIKdf = pd.DataFrame(data['cik_str'])
+            # RESETS INDEX
+            CIKdf = CIKdf.reset_index()
+            # PARSE DATAFRAME TO FIND MATCH WITH TICKER, DROPSna VALUES POST FILTER
+            CIK = CIKdf.where(CIKdf['ticker'] == ticker).dropna()
+            # GET THE CIK NUMBER FROM THE ONLY REMAINING ROW IN THE DATAFRAME
+            CIK_number = CIK['cik_str'].values[0]
+            # RETURNS CIK_NUMBER
+            return CIK_number
+        except:
+            print('Error: Ticker Not Found')
+
+    def getTickerFullName(self):
+        # CONVERTS THE INPUT TICKER TO ALL UPPERCASE
+        ticker = self.ticker.upper()
+        # TRY STATEMENT TO BETTER HANDLE ERRORS
+        try:
+            # GET CIK DATAFRAME, GETCIKDATAFRAME MUST BE DEFINED PRIOR TO CALL
+            data = self.getCIKDataFrame()
+            # CREATE DATAFRAME FROM TITLE ONLY
+            CIKdf = pd.DataFrame(data['title'])
+            # RESETS INDEX
+            CIKdf = CIKdf.reset_index()
+            print(CIKdf)
+            # PARSE DATAFRAME TO FIND MATCH WITH TICKER, DROPSna VALUES POST FILTER
+            CIK = CIKdf.where(CIKdf['ticker'] == ticker).dropna()
+            # GET THE CIK NUMBER FROM THE ONLY REMAINING ROW IN THE DATAFRAME
+            CIK_number = CIK['title'].values[0]
+            # RETURNS CIK_NUMBER
+            return CIK_number
+        except:
+            print('Error: Ticker Not Found')
+
+
+
+
+#######################################################################
+######################### FINANCIAL FUNCTIONS #########################
+
+    # RETURNS THE VALUE OF A TAG IN DATAFRAME FORMAT FOR TICKER
+    def getValueOfTag(self, tag):
+        CIKOfTicker = self.getTickerCIK()
+        response = requests.get(f"https://data.sec.gov/api/xbrl/companyconcept/CIK{CIKOfTicker}/us-gaap/{tag}.json", headers=headers)
+        lowerLvlDictKey = response.json()['units'].keys()
+        jsonUnitsChildKey = list(lowerLvlDictKey)[0]
+        valueOfTag = pd.json_normalize(response.json()["units"][jsonUnitsChildKey])
+        valueOfTag["filed"] = pd.to_datetime(valueOfTag["filed"])
+        valueOfTag = valueOfTag.sort_values("end")
+        return valueOfTag
+
+    def getAnnualValuesOfTag(self, tag):
+        valuesDF = self.getValueOfTag(tag)
+        
+        valuesDF = valuesDF.drop(columns='filed')
+        # valuesDF = valuesDF.drop(columns='accn')
+        # valuesDF = valuesDF.drop(columns='form')
+        # valuesDF = valuesDF.drop(columns='start')
+        # valuesDF = valuesDF.drop(columns='end')
+        # valuesDF = valuesDF.drop(columns='fy')
+        # # valuesDF = valuesDF.drop(columns='fp')
+        # valuesDF = valuesDF.drop(columns='frame')
+        valuesDFannualFilter = valuesDF.where(~valuesDF['fp'].str.contains("Q")).dropna(how='all')
+        valuesDFannualFilter = valuesDFannualFilter.where(~valuesDFannualFilter['frame'].str.contains("Q", na=False)).dropna()
+        print(valuesDFannualFilter)
+        # valuesJSON = valuesDFannualFilter.to_dict(orient='index')
+        return valuesDFannualFilter
+
+
+#####################################################################
+######################### PRICING FUNCTIONS #########################
 
     @lru_cache(maxsize=32)
     def getCurrentPrice(self):
@@ -67,9 +166,14 @@ class Equity:
         return volumedata
 
 
-###################################
-########## API CLASSES ############
-###################################
+###################################################################################################################
+################################################## API CLASSES ####################################################
+###################################################################################################################
+
+# ONLY API CLASSES SHOULD BE MAKING THE CONVERSION TO DICT TO THEN BE DISPLAYED IN JSON ON THE API ENDPOINT
+# THIS IS BECAUSE WE MIGHT NEED TO USE THE FUNCTIONS ELSE WHERE ESPECIALLY WHEN MAKING COMPARISIONS/CALCULATIONS
+
+########## PRICING DATA CLASSES ##########
 
 class YTDreturn(Resource):
     def get(self, ticker):
@@ -104,10 +208,60 @@ class volumeOneYear(Resource):
         volumeData.index = volumeData.index.strftime("%m/%d/%Y")
         volumeDataJson = volumeData.to_dict(orient='index')
         return volumeDataJson
+    
+
+########## MISC CLASSES ##########
+    
+class tickerCIK(Resource):
+    def get(self, ticker):
+        tickerCIK = Equity(ticker).getTickerCIK()
+        return tickerCIK
+    
+class tickerName(Resource):
+    def get(self, ticker):
+        tickerName = Equity(ticker).getTickerFullName()
+        return tickerName
+    
+        
+
+
+########## FINANCIAL STATEMENT CLASSES ##########
+
+class netIncomeLossAnnual(Resource):
+    def get(self, ticker):
+        tag = "NetIncomeLoss"
+        netIandL = Equity(ticker).getAnnualValuesOfTag(tag)
+        return netIandL
+
+class OpertatingCashflowsAnnual(Resource):
+    def get(self, ticker):
+        tag = "NetCashProvidedByUsedInOperatingActivities"
+        CFOannual = Equity(ticker).getAnnualValuesOfTag(tag).to_dict(orient='index')
+        return CFOannual
+    
+class CapitalExpenditureAnnual(Resource):
+    def get(self, ticker):
+        tag = "PaymentsToAcquirePropertyPlantAndEquipment"
+        CapexAnnual = Equity(ticker).getAnnualValuesOfTag(tag).to_dict(orient='index')
+        return CapexAnnual
+    
+
+###### help!!! ####
+class FreeCashFlowAnnual(Resource):
+    def get(self, ticker):
+        cfotag = "NetCashProvidedByUsedInOperatingActivities"
+        CfoAnnual = Equity(ticker).getAnnualValuesOfTag(cfotag)
+        capextag = "PaymentsToAcquirePropertyPlantAndEquipment"
+        CapexAnnual = Equity(ticker).getAnnualValuesOfTag(capextag)
+
+        ## NEEDS WORK DUE TO DIFFERENCE IN LENGTH OF BOTH COLUMNS
+        FreeCashFlows = CfoAnnual['val'] - CapexAnnual['val']
+        print(FreeCashFlows)
+        return 1
 
 
 
-#############
+########## EXPIRIMENTAL CLASSES ##########
 
 class Stock(Resource):
     def get(self, ticker):
@@ -134,20 +288,40 @@ class TreasuryData(Resource):
 
 
 
-###################################
-########## API ENDPOINTS ##########
-###################################
+#######################################################################################################################################
+############################################################ API ENDPOINTS ############################################################
+#######################################################################################################################################
 
 api.add_resource(status, '/')
 
-api.add_resource(YTDreturn, '/Stock/<ticker>/ytdreturn')
-api.add_resource(threeYearReturn, '/Stock/<ticker>/3Yreturn')
-api.add_resource(fiveYearReturn, '/Stock/<ticker>/5Yreturn')
+# EQUITY RETURNS ENDPOINTS
+api.add_resource(YTDreturn, '/Stock/<ticker>/returns/ytd')
+api.add_resource(threeYearReturn, '/Stock/<ticker>/returns/3y')
+api.add_resource(fiveYearReturn, '/Stock/<ticker>/returns/5y')
+
+# EQUITY DIVIDEND ENDPOINTS
 api.add_resource(dividends, '/Stock/<ticker>/dividends')
+
+# EQUITY PRICE ENDPOINTS
 api.add_resource(latestPrice, '/Stock/<ticker>/currentPrice')
+
+# EQUTIY VOLUME ENDPOINTS
 api.add_resource(volumeOneYear, '/Stock/<ticker>/volume/1y')
 
+# EQUITY MISC ENDPOINTS
 api.add_resource(Stock, '/Stock/<ticker>')
+api.add_resource(tickerCIK, '/Stock/<ticker>/ciknumber')
+api.add_resource(tickerName, '/Stock/<ticker>/cikname')
+
+# EQUITY FINANCIALS ENDPOINTS
+api.add_resource(netIncomeLossAnnual, '/Stock/<ticker>/annual/netincomeloss')
+api.add_resource(OpertatingCashflowsAnnual, '/Stock/<ticker>/annual/operatingcashflows')
+api.add_resource(CapitalExpenditureAnnual, '/Stock/<ticker>/annual/capex')
+api.add_resource(FreeCashFlowAnnual, '/Stock/<ticker>/annual/freecashflow')
+
+
+
+# TREASURY ENDPOINTS
 api.add_resource(TreasuryData, '/treasuries')
 
 
@@ -158,78 +332,3 @@ api.add_resource(TreasuryData, '/treasuries')
 
 if __name__ == '__main__':
     app.run()
-
-# import pandas as pd
-# import pymongo
-
-## PUSH 
-# git push heroku main
-# api.add_resource(FinanceTags, '/<ticker>')
-# api.add_resource(TagsByYear, '/<ticker>/<year>')
-
-
-
-
-
-
-
-# CLASS OBJECT TO HANDLE METHODS OF READING DATA FROM PYMONGO
-# class DataBase():
-#     # READING CONTENTS OF THE DATA BASE FOR SELECTED TICKER
-#     def getMasterFrame(self, input_ticker):
-
-#         ticker = input_ticker.upper()
-
-#         print(f'Searching database for {ticker}...')
-
-#         client = pymongo.MongoClient("mongodb+srv://ryanhhogan:1314red1314red@cluster0.t0ayqhj.mongodb.net/")
-
-#         client.list_database_names()
-
-#         stock = client[ticker]
-
-#         ann_tag_string = f'{ticker}_Annual_Tags'
-
-#         collection = stock[ann_tag_string]
-
-#         df_accounts = pd.DataFrame(list(collection.find()))
-
-#         print('Success')
-
-#         return df_accounts
-    
-#     # CONVERTS THE DATAFRAME RETRIEVED IN getMasterFrame FUNCTION AND CONVERTS IT TO JSON FOR API USE
-#     def ConvertMasterFrameToJSON(self, input_ticker):
-        
-#         annual_tags_df = self.getMasterFrame(input_ticker)
-
-#         annual_tags_df = annual_tags_df.drop(['_id'], axis=1)
-
-#         json_data = annual_tags_df.to_dict(orient='records')
-
-#         return jsonify(json_data)
-    
-#     def getTagsByYear(self, input_ticker, year):
-        
-#         annual_tags_df = self.getMasterFrame(input_ticker)
-
-#         annual_tags_df = annual_tags_df.drop(['_id'], axis=1)
-
-#         parse_by_year = annual_tags_df.where(annual_tags_df['frame'].str.contains(str(year)) & (~annual_tags_df['frame'].str.contains("Q"))).dropna(how='all')
-
-#         json_data = parse_by_year.to_dict(orient='records')
-
-#         return jsonify(json_data)
-
-
-# class FinanceTags(Resource):
-#     def get(self, ticker):
-#         db_get_request = DataBase()
-#         request_in_json = db_get_request.ConvertMasterFrameToJSON(ticker)
-#         return request_in_json
-    
-# class TagsByYear(Resource):
-#     def get(self, ticker, year):
-#         db_connection = DataBase()
-#         request_in_json = db_connection.getTagsByYear(ticker, year)
-#         return request_in_json
